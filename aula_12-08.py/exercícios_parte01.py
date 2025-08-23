0c019700663a364c5cf2c363fcbbace6519e76d7
danirfs22 <daniellerfs17@hotmail.com>
2025-08-22 22:04:29 -0300

    exercícios_parte01

---
+++ aula_12-08.py/exercícios_parte01.py
@@ -0,0 +1,17 @@
+print("Olá,mundo!")
+
+print("exibir")
+print("mensagens")
+print("múltiplas")
+
+print("Nome: João\nIdade: 20 anos\nCurso: Python Básico")
+
+nome = "João"
+idade = "20 anos"
+curso = "Python Básico"
+print(nome, idade, curso)
+
+print("####")
+print("#  #")
+print("#  #")
+print("####")
