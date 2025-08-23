e4ea1a696510f1b7bd3d7f7ff4261a13552fb4d4
danirfs22 <daniellerfs17@hotmail.com>
2025-08-22 22:01:15 -0300

    tipos

---
+++ aula_12-08.py/tipos.py
@@ -0,0 +1,18 @@
+print(type("22")) #string (texto)
+print(type(22.3)) #número - ponto flutuante (real)
+print(type(22)) #número - inteiro
+print(type(True)) #booleano (True/False) - comparações
+print(type(str(22)))
+# nome = input("Informe seu nome:")
+# print(type(int(nome)))
+# print(bool("sim"))
+
+nome = "Dani"
+cpf = 876728172
+residencia = "casa"
+
+print(f"Eu: {nome}, com cpf: {cpf}, residente no endereço: {residencia}...")
+
+nome = input("Digite seu nome:")
+print(f"O nome do usuario é:{nome}")
+
