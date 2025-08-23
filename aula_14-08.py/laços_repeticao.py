256a4e9243eb9dab14480e985c7fd5f6e544d891
danirfs22 <daniellerfs17@hotmail.com>
2025-08-22 22:19:32 -0300

    laços_repetição

---
+++ aula_14-08.py/laços_repeticao.py
@@ -0,0 +1,30 @@
+for i in range(-1,-5, -1):
+    print(i)
+
+for fruta in ["pera", "limao"]:
+    print(fruta)
+'''
+count = 0
+while True:
+    print("Dentro do while")
+    count+=1
+    if count == 4:
+         break
+'''
+
+'''
+count = 0
+while count < 4:
+    print("Dentro do while")
+    count+=1
+  ''' 
+'''
+count = 0
+while count <= 10:
+       count+=1
+    if count == 4:
+        continue
+     print(count)
+else:
+print("terminou o while")
+'''
