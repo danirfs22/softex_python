88ae1eb2e8571ba384f2152b072a5fea596073d7
danirfs22 <daniellerfs17@hotmail.com>
2025-08-22 21:56:59 -0300

    condicionais

---
+++ aula_12-08.py/condicionais.py
@@ -0,0 +1,10 @@
+x = int(input("Informe o primeiro numero:"))
+y = int(input("Informe o segundo numero:"))
+operacao = input("Informe a operaçao")
+
+if  y !=0:
+    print (x/y)
+elif True:
+    print ("Ok")
+else:
+    print("O valor informado para o segundo numero tem que ser diferente de 0")
