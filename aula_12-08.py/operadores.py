3e201811b45d8cf6a511769c899844adf17392b2
danirfs22 <daniellerfs17@hotmail.com>
2025-08-22 21:59:04 -0300

    operadores

---
+++ aula_12-08.py/operadores.py
@@ -0,0 +1,28 @@
+print(3%2) #resto
+print(3+2)
+print(3-2)
+print(3*2)
+print(10/3)#divisao tradicional (ponto flutuante)
+print(10//3) #divisao inteira
+print(3**2) 
+print(2**2**3)
+
+x = 4
+y = 4.
+print (x == y)
+
+x = "4"
+y = "4"
+print (x + y) #soma de strings = concatenação = 44 - só funciona com números inteiros
+
+x = 4
+y = "4"
+print (x * y) #multiplica a string a quantidade de vezes especificada = 4444 (replicação) - só funciona com números inteiros
+
+x = 4
+y = 4
+print (x / y)
+
+x = 5
+y = 4
+print (x != y) #diferente
