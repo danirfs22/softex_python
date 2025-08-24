f1295595f47e694f13514373059c05a10fd78c6e
danirfs22 <daniellerfs17@hotmail.com>
2025-08-23 21:12:26 -0300

    exercicios_condicionais

---
+++ aula_12-08.py/exercicios_condicionais.py
@@ -0,0 +1,44 @@
+num = int(input("Digite um número:"))
+if num%2==0:
+    print("Esse número é par!")
+else:
+    print("Esse número é ímpar!")
+
+nota = float(input("Digita a nota do aluno:"))
+
+if nota<5:
+    print("Reprovado")
+elif nota>=7:
+    print("Aprovado")
+else:
+    print("Recuperação")
+    
+num1 = float(input("Digite o primeiro número:"))
+num2 = float(input("Digite o segundo número:"))
+
+if num1>num2:
+    print("O primeiro número é maior")
+else:
+    print("O segundo número é maior")
+if num1==num2:
+    print("os números são iguais!")
+
+idade = int(input("Digite sua idade:"))
+if idade>=65:
+    print("Idoso")
+elif idade<=12:
+    print("Criança")
+elif idade>=18:
+     print("Adulto")
+else:
+    print("Adolescente")
+
+
+
+
+
+
+
+
+
+
