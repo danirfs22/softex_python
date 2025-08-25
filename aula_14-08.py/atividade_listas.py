8debd26d050a7997b45b1c14f07956a7882fba4a
danirfs22 <daniellerfs17@hotmail.com>
2025-08-25 14:28:09 -0300

    atividade_listas "sala"

--- aula_14-08.py/atividade_listas.py
+++ aula_14-08.py/atividade_listas.py
@@ -1,8 +1,23 @@
-nomes = ["ana", "maria", "joana"]
-lista_1 = ["dani", 3]
-lista_1+= nomes
+lista_1 = ["Ana", "Maria", "Joana", 3, ["azul, rosa"]]
 print(lista_1)
 
-lista_1.append("josé","joão")
-print(lista_1)            
+lista_1.insert(2, "Dani")
+lista_1.append("vermelho")
+print(lista_1)
+
+lista_1.remove("Ana")
+print(lista_1)
+
+lista_2 = lista_1.copy
+print(id(lista_1))
+print(id(lista_2))
+
+num = [2,7,3.5,24]
+i=1
+for i in num:
+    print(i*5)
+
+lista_num = [1,2,3,4,5,6]
+print(lista_num[3:5])
+
 
--- aula_21-08.py/matriz.py
+++ aula_21-08.py/matriz.py
@@ -1,4 +1,4 @@
-matriz2x2 = [1,2], [3,4]
+matriz2x2 = [[1.2], [3,4]]
 matriz3x2 = [[1,2],
              [3,4],
              [5,6]]
@@ -6,17 +6,17 @@ print(matriz[1])
 print(matriz[1][2]) #primeiro o indice, depois o elemento
 matriz[1][2] = "a"
 
-matriz com list comprehension
+#matriz com list comprehension
 
-matriz = [0 for i in range(3)] for x in range(3) #primeiro item é o valor que quer adicionar
+matriz = [[0 for i in range(3)] for x in range(3)] #primeiro item é o valor que quer adicionar
 print(matriz)
 
-matriz = [i for i in range(3)] for x in range(3):
-    lista = [1,2,3]
-    matriz = [i for i in lista] for x in range(3):
+matriz = [[i for i in range(3)] for x in range(3)]
+lista = [1,2,3]
+matriz = [[i for i in lista] for x in range(3)]
         
-        matriz2 = []
-        for i in range(3):
+matriz2 = []
+for i in range(3):
             sublista=[]
             matriz2.append(sublista)
             for x in range(3):
