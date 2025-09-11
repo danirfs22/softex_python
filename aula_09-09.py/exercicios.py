90139b31a7c3b836f45db654d687a004a2a41409
danirfs22 <121183358+danirfs22@users.noreply.github.com>
2025-09-11 17:45:34 +0000

    Exercícios POO

--- aula_09-09.py/exercicios.py
+++ aula_09-09.py/exercicios.py
@@ -24,22 +24,22 @@
 
 # 3 - Crie uma classe Carro com os atributos marca, modelo e ano. Use o método __init__ para inicializar esses valores. Crie três objetos e mostre suas informações
 
-class Carro:
-    def __init__(self, marca, modelo, ano):
-        self.marca = marca
-        self.modelo = modelo
-        self.ano = ano
+# class Carro:
+#     def __init__(self, marca, modelo, ano):
+#         self.marca = marca
+#         self.modelo = modelo
+#         self.ano = ano
 
-    def informacoes(self):
-        print(f"Marca: {self.marca}, Modelo: {self.modelo}, Ano: {self.ano}")
+#     def informacoes(self):
+#         print(f"Marca: {self.marca}, Modelo: {self.modelo}, Ano: {self.ano}")
     
-carro01 = Carro("Chevrolet", "Onix", 2025)
-carro02 = Carro("Fiat", "Uno", 2022)
-carro03 = Carro("Renault", "Logan", 2024)
+# carro01 = Carro("Chevrolet", "Onix", 2025)
+# carro02 = Carro("Fiat", "Uno", 2022)
+# carro03 = Carro("Renault", "Logan", 2024)
 
-carro01.informacoes()
-carro02.informacoes()
-carro03.informacoes()
+# carro01.informacoes()
+# carro02.informacoes()
+# carro03.informacoes()
 
 # #4 - Usando a classe Carro, crie um objeto e depois altere o valor de um de seus atributos (por exemplo, mudar o ano). Imprima antes e depois da alteração.
 # class Carro:
@@ -48,3 +48,39 @@ carro03.informacoes()
 #         self.modelo = modelo
 #         self.ano = ano
 
+#5 - Crie uma classe ContaBancaria com os atributos titular e saldo. Adicione um método depositar(valor) que aumenta o saldo e um método sacar(valor) que diminui o saldo (se houver saldo suficiente). Teste com depósitos e saques.
+class ContaBancaria:
+   
+    def __init__(self, titular, saldo=0):
+        self.titular = titular
+        self.saldo = saldo
+        self.operacoes = []
+
+    def __str__(self):
+        return f"Conta do titular {self.titular} tem {self.saldo} de saldo"
+    
+    def deposito(self, valor):
+        self.saldo += valor
+        self.operacoes.append(["Deposito", valor])
+        print(f"Foi depositado {valor} reais na sua conta")
+    
+    def saque(self, valor):
+        self.saldo -= valor
+        self.operacoes.append(["Saque", valor])
+        print(f"Foi sacado {valor} reais na sua conta")
+
+    def extrato(self):
+        for operacao in self.operacoes:
+            print(operacao)
+        print(f"Saldo: {self.saldo}")
+
+conta1 = ContaBancaria("João",60)
+conta2 = ContaBancaria("Bruna", 1000)
+
+print(conta1)
+print(conta2)
+
+conta1.deposito(300)
+conta1.saque(100)
+conta2.saque(250)
+conta1.extrato()
